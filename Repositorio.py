d28db6b672b4334939a6a0f4da4a10465c318675
Felipe <sfelipe9506@gmail.com>
2024-11-21 19:43:39 -0500

    segundo commit

---
+++ Repositorio.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name: str, phone: str, email: str, surmane: str):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.surname = surname 
+
+    def __str__(self):
